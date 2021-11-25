d4079c4232328af6fdb5e48cfbdf1817f931dc3c
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-11-25 16:04:19 +0700

    Add Step Button Animation

---
+++ Assets/Scripts/Animation/StepOnAnimator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepOnAnimator : MonoBehaviour
+{
+    public Animator anim;
+    public bool objectInRange;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (objectInRange)
+        {
+            anim.SetBool("stepIN", true);
+        }
+        else
+        {
+            anim.SetBool("stepIN", false);
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Pushable"))
+        {
+            objectInRange = true;
+            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Pushable"))
+        {
+            objectInRange = false;
+            
+        }
+    }
+}
