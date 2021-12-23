31db4e46b164b4016ba0c8fdd8ae6e66cde41a57
Dandi Prayogatama <72259228+dandirayo@users.noreply.github.com>
2021-12-23 07:35:05 +0700

    Test

---
+++ Assets/Scripts/UI/FullScreenTog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullScreenTog : MonoBehaviour
+{
+    public void Fullscene(bool is_fullscene)
+    {
+        Screen.fullScreen = is_fullscene;
+        Debug.Log("is Full Screen");
+    }
+
+}
---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+//1 Turn On PauseMenu
+//2 Stop-Time Completely
+//3 Pause = true
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false ;
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        //to -> Main Menu Scene(belum)
+        Debug.Log("Loading Menu.....");
+
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); //buildIndex + 1 atau + "MainMenu"
+       // Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quiting Game...");
+    }
+
+}
