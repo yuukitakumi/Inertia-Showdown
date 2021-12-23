2d1a114681e74b6d88a9d7a8528ba8bfab887d37
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-12-24 06:30:45 +0700

    add 3 new Scene Past
    Gudang 1, Gudang 2, Ruang Sebelah

---
+++ Assets/Scripts/OpenSpike.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenSpike : MonoBehaviour
+{
+    public int counterNeeded;
+    public GameManager gameManager;
+    public bool isDone;
+    public Animator animGate;
+    public Collider2D collider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameManager.counter == counterNeeded)
+        {
+            isDone = true;
+            animGate.SetBool("isOpen", true);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else
+        {           
+            animGate.SetBool("isOpen", false);
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }    
+    }
+}
