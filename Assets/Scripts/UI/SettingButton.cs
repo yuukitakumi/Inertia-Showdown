aeacd1c20fa80225f3185a07c3ad7e191e0ece99
Dandi Prayogatama <72259228+dandirayo@users.noreply.github.com>
2021-12-24 21:59:12 +0700

    Pause & Setting Panel

---
+++ Assets/Scripts/UI/SettingButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingButton : MonoBehaviour
+{
+    public  GameObject panelSetting;
+    public GameObject panelPause;
+
+    public void OpenPanel()
+    {
+        if(panelSetting)
+        {
+            panelSetting.SetActive(true);
+        }
+        else
+        {
+            panelPause.SetActive(false);
+        }
+    }
+
+    
+}
