11c2671c5cad71fd37ba6c7f35d175e4e07ade2a
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2021-12-27 05:10:23 +0700

    Benerin Animasi Pintu

---
+++ Assets/Scripts/SwitchClick.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchClick : MonoBehaviour
+{
+    [SerializeField] public GameObject interactUI;
+
+    public bool PlayerInRange;
+    public bool isDone;
+    public GameManager gameManager;
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && PlayerInRange)
+        {
+            isDone = true;
+            gameManager.counter++;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.SetActive(true);
+            PlayerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.SetActive(false);
+            PlayerInRange = false;
+        }
+    }
+}
