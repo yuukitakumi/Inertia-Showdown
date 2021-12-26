fa8bc2e7c9115f6bf1f45958e61df28d14bf7050
hkldtrinanda <hkldtrinanda12@gmail.com>
2021-12-26 22:42:12 +0700

    gatau keberapa

---
+++ Assets/Scripts/UI/LoaderScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoaderScene : MonoBehaviour 
+{
+    public Transform Loadbar;
+    [SerializeField]
+    private float now, speed;
+
+    void Update ()
+    {
+        if (now < 100)
+        {
+            now += speed * Time.deltaTime;
+            Debug.Log((int)now);
+        }
+        else
+        {
+            Application.LoadLevel ("mainmenu");
+        }
+        Loadbar.GetComponent<Image>().fillAmount = now/100;
+    }
+}
