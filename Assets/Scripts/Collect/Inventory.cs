57e91b7778076d9033ec20c6189064448c13d505
agumon69 <yusril.olan@gmail.com>
2021-12-27 22:52:22 +0800

    Update Inventory.cs

--- Assets/Scripts/Collect/Inventory.cs
+++ Assets/Scripts/Collect/Inventory.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Inventory : MonoBehaviour
 {
-    public Text noteCounter;
+    public TextMeshProUGUI noteCounter;
     public Text keyCounter;
 
     private int notes = 0;
