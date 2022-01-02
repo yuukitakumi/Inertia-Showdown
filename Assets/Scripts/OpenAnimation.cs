d8eaafdddce246c6165d817fda903602ce07e555
Muhammad Aulia Rahman <meehchannel7@gmail.com>
2022-01-02 19:47:05 +0700

    Add Scene Transition Past

---
+++ Assets/Scripts/OpenAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAnimation : MonoBehaviour
+{
+    public int counterNeeded;
+    public GameManager gameManager;
+
+    public Animator animHantu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameManager.counter == counterNeeded)
+        {
+            animHantu.SetBool("IsOpen", true);
+        }
+    }
+}
