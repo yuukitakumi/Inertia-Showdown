2068bdb7c0fa654f9dbe3e50c90c1db61f0f1731
hkldtrinanda <hkldtrinanda12@gmail.com>
2022-01-12 01:41:16 +0700

    minor

---
+++ Assets/HilangPatung.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HilangPatung: MonoBehaviour
+{
+    
+    [SerializeField] public GameObject interactUI;
+    
+    public bool PlayerInRange;
+    public GameObject komponen;
+    public BoxCollider2D box;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && PlayerInRange)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            gameObject.SetActive(false);
+        }
+       
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.SetActive(true);
+            PlayerInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.SetActive(false);
+            PlayerInRange = false;
+        }
+    }
+}
