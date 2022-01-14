b163a7850eb36cab961111c2a63cb622428ad4ed
Jaska1405 <jaskamanurung2001@gmail.com>
2022-01-14 16:57:27 +0700

    puzzle
    sementara

---
+++ Assets/Scripts/GambarArtifak/FeedBackArtifak.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedBackArtifak : MonoBehaviour
+{
+    public GameObject feedback;
+    bool selesai = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void cek()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (transform.GetChild(i).GetComponent<DragArtifak>().on_tempel)
+            {
+                selesai = true;
+            }
+            else
+            {
+                selesai = false;
+                i = 5;
+            }
+        }
+        if (selesai)
+        {
+            feedback.SetActive(true);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!selesai)
+        {
+            cek();
+        }
+    }
+}
