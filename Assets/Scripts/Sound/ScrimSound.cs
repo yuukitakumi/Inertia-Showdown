5fccbe87712600a17d8007caa9e71a04e3060c17
Dandi Prayogatama <72259228+dandirayo@users.noreply.github.com>
2022-01-14 22:17:02 +0700

    Update Sound sampe Last

---
+++ Assets/Scripts/Sound/ScrimSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrimSound : MonoBehaviour
+{
+    public AudioSource screamSound;
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        screamSound.Play();
+    }
+        
+    
+}
