4a2bbf4ab58a9e60fb34209c04dd8b33edfe1bec
hkldtrinanda <hkldtrinanda12@gmail.com>
2022-01-19 18:52:09 +0700

    revisi

---
+++ Assets/InjekanLibraryPast.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InjekanLibraryPast : MonoBehaviour
+{
+    
+    public GameManager1 gameManager;
+
+    public void Start()
+    {
+        gameManager = FindObjectOfType<GameManager1>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")  == true)
+            
+            gameManager.counter++;
+    }
+    
+}
--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public int counter;
-    public void Start()
-    {
-        DontDestroyOnLoad(this);
-    }
+
    
 }
